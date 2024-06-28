d8ab715c2afac8c9c6aa86c646f56679b06246a6
Lucas-L0w <169653287+Lucas-L0w@users.noreply.github.com>
2024-06-28 15:14:01 +0100

    outed sqlachemy

--- target_mssql/connector.py
+++ target_mssql/connector.py
@@ -51,26 +51,26 @@ class mssqlConnector(SQLConnector):
             full_table_name=full_table_name, schema=schema, records=records
         )
 
-    def get_sqlalchemy_url(self, config: dict) -> str:
-        """Generates a SQLAlchemy URL for mssql.
+    # def get_sqlalchemy_url(self, config: dict) -> str:
+    #     """Generates a SQLAlchemy URL for mssql.
 
-        Args:
-            config: The configuration for the connector.
-        """
+    #     Args:
+    #         config: The configuration for the connector.
+    #     """
 
-        if config.get("sqlalchemy_url"):
-            return config["sqlalchemy_url"]
+    #     if config.get("sqlalchemy_url"):
+    #         return config["sqlalchemy_url"]
 
-        connection_url = sqlalchemy.engine.url.URL.create(
-            drivername="mssql+pymssql",
-            username=config["username"],
-            password=config["password"],
-            host=config["host"],
-            port=config["port"],
-            database=config["database"],
-        )
+    #     connection_url = sqlalchemy.engine.url.URL.create(
+    #         drivername="mssql+pymssql",
+    #         username=config["username"],
+    #         password=config["password"],
+    #         host=config["host"],
+    #         port=config["port"],
+    #         database=config["database"],
+    #     )
 
-        return str(connection_url)
+    #     return str(connection_url)
 
 
     def create_empty_table(
